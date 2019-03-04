60337f719cb681576e38d612411f5ca7df137385
David Kent <dekent90@gmail.com>
2019-03-04 12:14:32 -0500

    Visualization changes for static figures

--- include/waypoint_planner/HumanSimulator.h
+++ include/waypoint_planner/HumanSimulator.h
@@ -12,6 +12,7 @@
 #include <geometry_msgs/Vector3.h>
 #include <tf2_ros/transform_broadcaster.h>
 #include <std_msgs/Float32.h>
+#include <visualization_msgs/MarkerArray.h>
 
 #include "waypoint_planner/EnvironmentSetup.h"
 
@@ -28,9 +29,14 @@ private:
 
   void timeUpdateCallback(const std_msgs::Float32::ConstPtr& msg);
 
+  void createTaskMarkers(int task);
+
+  void addHumanMarker(std::string frame, double r, double g, double b);
+
   ros::NodeHandle n, pnh;
 
   ros::Publisher human_marker_publisher;
+  ros::Publisher task_markers_publisher;
   ros::Subscriber time_update_subscriber;
 
   tf2_ros::TransformBroadcaster tf_broadcaster;
@@ -40,6 +46,11 @@ private:
   double speed_factor;
 
   visualization_msgs::Marker human_marker;
+  visualization_msgs::MarkerArray task_markers;
+
+  geometry_msgs::TransformStamped task_tf1;
+  geometry_msgs::TransformStamped task_tf2;
+  geometry_msgs::TransformStamped task_tf3;
 };
 
 #endif  // WAYPOINT_PLANNER_HUMAN_SIMULATOR_H_
