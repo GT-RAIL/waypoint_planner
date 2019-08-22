29191c7449928394e12897faadaa58000dc73a4f
David Kent <dekent90@gmail.com>
2019-08-22 14:18:04 -0400

    Balanced class sampling from dataset

--- scripts/Train.py
+++ scripts/Train.py
@@ -12,6 +12,7 @@ import rospy
 
 from PolicyModel import Conv3DPolicy
 from WaypointPolicyDataset import WaypointPolicyDataset
+from WaypointDecisionDataset import WaypointDecisionDataset
 
 use_cuda = torch.cuda.is_available()
 if use_cuda:
@@ -22,20 +23,52 @@ device = torch.device('cuda:0' if use_cuda else 'cpu')  # change to 'cuda:0' wit
 
 max_epochs = 1000
 
+# num_classes = 32
+num_classes = 2
+
 # Training data
 datapath = rospkg.RosPack().get_path('waypoint_planner') + '/data/training/trajectories/'
 csvpath = rospkg.RosPack().get_path('waypoint_planner') + '/data/training/all_samples.csv'
 print('Creating training dataset from:', csvpath)
 
-training_set = WaypointPolicyDataset(csv_file=csvpath, root_dir=datapath)
-training_loader = DataLoader(training_set, batch_size=4, shuffle=True)
+training_set = WaypointDecisionDataset(csv_file=csvpath, root_dir=datapath)
+print('Training set created.')
 
-# Validation data
+class_count = [0]*num_classes
+idx = 0
+for item, label in training_set:
+    class_count[label] += 1
+    idx += 1
+class_weight = [0]*num_classes
+total_samples = float(sum(class_count))
+print('Counts:', class_count)
+for i in range(num_classes):
+    if class_count[i] == 0:
+        class_weight[i] = 0
+    else:
+        class_weight[i] = total_samples/float(class_count[i])
+sample_weight = [0]*len(training_set)
+idx = 0
+print('Class weights calculated:', class_weight)
+for item, label in training_set:
+    sample_weight[idx] = class_weight[label]
+    idx += 1
+print('Sample weights calculated.')
+sample_weight_tensor = torch.DoubleTensor(sample_weight)
+sampler = torch.utils.data.sampler.WeightedRandomSampler(sample_weight_tensor, len(sample_weight_tensor), replacement=False)
+
+# TODO: set a lower number of samples so that the weighted sampler without replacement samples balanced classes
+num_samples = min(class_count)  # Samples to use for each batch (cover all of the least-seen class each epoch)
+
+# training_loader = DataLoader(training_set, batch_size=4, shuffle=True)
+training_loader = DataLoader(training_set, batch_size=4, shuffle=False, sampler=sampler)
+
+# # Validation data
 val_datapath = rospkg.RosPack().get_path('waypoint_planner') + '/data/testing/trajectories/'
 val_csvpath = rospkg.RosPack().get_path('waypoint_planner') + '/data/testing/all_samples.csv'
 print('Creating validation dataset from:', val_csvpath)
 
-validation_set = WaypointPolicyDataset(csv_file=val_csvpath, root_dir=val_datapath)
+validation_set = WaypointDecisionDataset(csv_file=val_csvpath, root_dir=val_datapath)
 validation_loader = DataLoader(validation_set, batch_size=4, shuffle=False)
 
 # Stats
@@ -43,7 +76,7 @@ training_loss = []
 validation_loss = []
 
 print('Creating model...')
-model = Conv3DPolicy(num_classes=32)
+model = Conv3DPolicy(num_classes=num_classes)
 loss_fn = torch.nn.CrossEntropyLoss()
 learning_rate = .001
 # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -114,4 +147,4 @@ for epoch in range(max_epochs):
     #     print('Epoch:', epoch, '- validation loss:', validation_loss[len(training_loss) - 1])
 
     # scheduler.step()
-print('Done')
+# print('Done')
